e435e165ffea2d5c89c506c57d50e4438499b14f
terrence.gonsalves <terrence.gonsalves@gmail.com>
2026-03-24 07:57:52 -0400

    created placeholder files as we build out tabs

---
+++ app/(tabs)/activity.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function Activity() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Activity coming soon</Text>
+        </View>
+    );
+}
---
+++ app/(tabs)/checkin.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function CheckIn() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Check-in coming soon</Text>
+        </View>
+    );
+}
