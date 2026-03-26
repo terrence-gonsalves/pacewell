42681ef03ffef9c9ff641f3a3832829ed38b56c7
terrence.gonsalves <terrence.gonsalves@gmail.com>
2026-03-26 15:02:45 -0400

    added a view all insights

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -71,6 +71,7 @@ export default function RootLayout() {
             <Stack.Screen name="(auth)" />
             <Stack.Screen name="(tabs)" />
             <Stack.Screen name="edit-profile" />
+            <Stack.Screen name="insights" />
         </Stack>
     );
 }
