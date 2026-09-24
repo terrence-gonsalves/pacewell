79fc1f26778dc94e1875a51f9ef4c35b2758fc7a
terrence.gonsalves <terrence.gonsalves@gmail.com>
2026-09-24 14:09:10 -0600

    added the logo component

---
+++ components/logo/PacewellLogo.tsx
@@ -0,0 +1,32 @@
+import { Image, ImageStyle, StyleProp } from 'react-native';
+
+type PacewellLogoProps = {
+    size?: number;
+    variant?: 'green' | 'white';
+    style?: StyleProp<ImageStyle>;
+};
+
+export default function PacewellLogo({
+    size = 40,
+    variant = 'green',
+    style,
+}: PacewellLogoProps) {
+    const source =
+        variant === 'white'
+            ? require('../assets/branding/pacewell-logo-white-transparent.png')
+            : require('../assets/branding/pacewell-logo-green-transparent.png');
+
+    return (
+        <Image
+            source={source}
+            style={[
+                {
+                    width: size,
+                    height: size,
+                },
+                style,
+            ]}
+            resizeMode="contain"
+        />
+    );
+}
